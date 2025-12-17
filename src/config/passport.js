fea9826311c3e89dcca10549cb1814121b6d0978
sthalin <sthalin.11@gmail.com>
2025-12-17 12:46:07 -0500

    se agrego google auth 2

--- src/config/passport.js
+++ src/config/passport.js
@@ -85,6 +85,7 @@ if (GOOGLE_CLIENT_ID && GOOGLE_CLIENT_SECRET) {
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
         callbackURL: GOOGLE_CALLBACK_URL,
+        proxy: true,
     }, async (accessToken, refreshToken, profile, done) => {
 
         console.log("\n=============================");
